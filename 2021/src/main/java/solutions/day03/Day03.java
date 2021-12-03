a78a7cd3215074ff07e24093fd600c5552371581
JoshPavBjss <josh.paveley@bjss.com>
2021-12-03 09:00:11 +0000

    Day 3 done

--- 2021/src/main/java/solutions/day03/Day03.java
+++ 2021/src/main/java/solutions/day03/Day03.java
@@ -75,9 +75,31 @@ public class Day03 extends BaseDay {
 
     }
 
-    private int getCo2ScrubberRating() {
-        return 0;
+    private int getCo2ScrubberRating(final List<String> input) {
 
+        List<String> oxygenGeneratorRatings = new ArrayList<>(input);
+        int index = 0;
+        while (oxygenGeneratorRatings.size() != 1) {
+            final String gammaRateBytes = getGammaRate(oxygenGeneratorRatings);
+
+            int mask = (1 << gammaRateBytes.length()) - 1;
+            final int gammaRate = Integer.parseInt(gammaRateBytes, 2);
+
+            StringBuilder epsilonRate = new StringBuilder(Integer.toBinaryString(gammaRate ^ mask));
+
+            while (epsilonRate.length() < gammaRateBytes.length()) {
+                epsilonRate.insert(0, '0');
+            }
+
+            final char leastCommonDigit = epsilonRate.charAt(index);
+            int finalIndex = index;
+            oxygenGeneratorRatings = oxygenGeneratorRatings.stream()
+                    .filter(s -> s.charAt(finalIndex) == leastCommonDigit)
+                    .collect(Collectors.toList());
+            index++;
+        }
+
+        return Integer.parseInt(oxygenGeneratorRatings.get(0), 2);
     }
 
     @Override
@@ -86,7 +108,8 @@ public class Day03 extends BaseDay {
         final List<String> input = getInputAsList();
 
         final int oxygenGeneratorRating = getOxygenGeneratorRating(input);
+        final int co2 = getCo2ScrubberRating(input);
 
-        return null;
+        return String.valueOf(oxygenGeneratorRating * co2);
     }
 }
