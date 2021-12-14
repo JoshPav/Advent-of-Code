1546051d969f39cfcca246c77ae3ff5b194987de
JoshPav <joshpaveley@gmail.com>
2021-12-14 07:13:19 +0000

    14 setup

---
+++ 2021/src/main/java/solutions/day14/Day14.java
@@ -0,0 +1,22 @@
+package solutions.day14;
+
+import solutions.BaseDay;
+
+import java.util.List;
+
+public class Day14 extends BaseDay {
+
+    public Day14(List<String> input) {
+        super(input);
+    }
+
+    @Override
+    public String solvePartOne() {
+        return null;
+    }
+
+    @Override
+    public String solvePartTwo() {
+        return null;
+    }
+}
---
+++ 2021/src/test/java/solutions/day14/Day14Test.java
@@ -0,0 +1,41 @@
+package solutions.day14;
+
+import org.junit.Test;
+import solutions.BaseTest;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static shared.FileUtils.getInput;
+
+public class Day14Test extends BaseTest {
+
+    @Test
+    public void testPartOne() {
+
+        assertEquals(null, getDayInstance(getTestInput()).solvePartOne());
+    }
+
+    @Test
+    public void testPartOneActualInput() {
+
+        assertEquals(null, getDayInstance(getInput(14)).solvePartOne());
+    }
+
+    @Test
+    public void testPartTwo() {
+
+        assertEquals(null, getDayInstance(getTestInput()).solvePartTwo());
+    }
+
+    @Test
+    public void testPartTwoActualInput() {
+
+        assertEquals(null, getDayInstance(getInput(14)).solvePartTwo());
+    }
+
+    private List<String> getTestInput() {
+        return List.of();
+    }
+
+}
