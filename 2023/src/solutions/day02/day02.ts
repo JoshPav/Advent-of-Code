f272a54e1d4242f1bb306aa9f8dbb6eaa7fbce10
Josh Paveley <josh.paveley@aula.ac.uk>
2023-12-02 08:11:31 +0000

    Slight cleanup

--- 2023/src/solutions/day02/day02.ts
+++ 2023/src/solutions/day02/day02.ts
@@ -1,63 +1,48 @@
 import { Day } from '../../types/day';
 import { sum } from '../../utils/reducers';
 
-type DrawnCubes = {
-  red: number;
-  green: number;
-  blue: number;
+type Cubes = {
+  red?: number;
+  green?: number;
+  blue?: number;
 };
 
 type Game = {
   id: string;
-  cubesRevealed: DrawnCubes[];
+  cubesRevealed: Cubes[];
 };
 
-const parseRound = (round: string): DrawnCubes => {
-  const cubesDrawn = round.split(',');
-
-  const drawnCubes: DrawnCubes = {
-    red: 0,
-    green: 0,
-    blue: 0,
-  };
-
-  cubesDrawn.forEach((drawn) => {
-    const [count, color] = drawn.trim().split(' ');
-
-    drawnCubes[color] = Number(count);
-  });
-
-  return drawnCubes;
+const parseRound = (round: string): Cubes => {
+  return Object.fromEntries(
+    round.split(',').map((drawn) => {
+      const [count, color] = drawn.trim().split(' ');
+      return [color, Number(count)];
+    }),
+  );
 };
 
 const parseLine = (line: string): Game => {
   const [id, details] = line.split(':');
 
-  const rounds = details.split(';');
-
   return {
     id: id.replace('Game ', ''),
-    cubesRevealed: rounds.map(parseRound),
+    cubesRevealed: details.split(';').map(parseRound),
   };
 };
 
-const getTotalCubesForColor = (color: keyof DrawnCubes, game: Game): number => {
-  return game.cubesRevealed.map((revealed) => revealed[color]).reduce(sum, 0);
-};
-
 const isGamePossible =
-  (total: DrawnCubes) =>
+  (total: Cubes) =>
   (game: Game): boolean => {
     return game.cubesRevealed.every((round) => {
       return (
-        round.red <= total.red &&
-        round.blue <= total.blue &&
-        round.green <= total.green
+        (!round.red || round.red <= total.red) &&
+        (!round.blue || round.blue <= total.blue) &&
+        (!round.green || round.green <= total.green)
       );
     });
   };
 
-const getMinRequired = (game: Game): DrawnCubes => {
+const getMinRequired = (game: Game): Cubes => {
   return game.cubesRevealed.reduce(
     (prev, curr) => {
       const updated = { ...prev };
@@ -84,23 +69,22 @@ const getMinRequired = (game: Game): DrawnCubes => {
   );
 };
 
-const getCubePowers = ({ red, blue, green }: DrawnCubes): number => {
-  return red * blue * green;
-};
+const getCubePowers = ({ red, blue, green }: Cubes): number =>
+  red * blue * green;
 
 export default {
   solvePartOne: (input: string[]): string | number => {
-    const games = input.map(parseLine);
-
-    const totalAllowed: DrawnCubes = {
+    const totalAllowed: Cubes = {
       red: 12,
       green: 13,
       blue: 14,
     };
 
-    const possible = games.filter(isGamePossible(totalAllowed));
-
-    return possible.map(({ id }) => Number(id)).reduce(sum, 0);
+    return input
+      .map(parseLine)
+      .filter(isGamePossible(totalAllowed))
+      .map(({ id }) => Number(id))
+      .reduce(sum, 0);
   },
   solvePartTwo: (input: string[]): string | number => {
     return input
