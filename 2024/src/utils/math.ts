01cc6ba26e1e17250b1d3409a4f3181225381561
Josh Paveley <josh.paveley@bjss.com>
2024-12-02 12:48:37 +0000

    more utils

--- 2024/src/utils/math.ts
+++ 2024/src/utils/math.ts
@@ -4,8 +4,11 @@ const greatestCommonDivisor = (a: number, b: number) =>
 export const leastCommonMultiple = (a: number, b: number) =>
   (a * b) / greatestCommonDivisor(a, b);
 
-export const allPositive = (arr: number[]) => arr.every((x) => x > 0);
-export const allNegative = (arr: number[]) => arr.every((x) => x < 0);
+export const isPositive = (x: number) => x > 0
+export const isNegative = (x: number) => x < 0
+
+export const allPositive = (arr: number[]) => arr.every(isPositive);
+export const allNegative = (arr: number[]) => arr.every(isNegative);
 
 export const getDifferences = (
   arr: number[],
