21522977daf8ba60a6996e5c79962a610d2fb250
Josh Paveley <josh.paveley@bjss.com>
2024-12-11 19:37:56 +0000

    Day 11: Remove pt1 code

--- 2024/src/solutions/day11/day11.ts
+++ 2024/src/solutions/day11/day11.ts
@@ -1,31 +1,12 @@
 import { Day } from '../../types/day';
 import { parseNumbers } from '../../utils/parsing';
-import { groupByCount, sum } from '../../utils/reducers';
+import { sum } from '../../utils/reducers';
 
 const numberOfDigits = (num: number) => `${num}`.length;
 
 const splitInHalf = (str: string) => {
   const half = str.length / 2;
   return [str.slice(0, half), str.slice(half)];
-};
-
-const processStone = (stone: number): number[] => {
-  // If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
-  if (stone === 0) {
-    return [1];
-  }
-
-  /* 
-    If the stone is engraved with a number that has an even number of digits, it is replaced by two stones. 
-    The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone. 
-  */
-  if (numberOfDigits(stone) % 2 === 0) {
-    const [l, r] = splitInHalf(`${stone}`);
-    return [parseInt(l), parseInt(r)];
-  }
-
-  // If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone.
-  return [stone * 2024];
 };
 
 const add = (map: Map<number, number>) => (num: number, count: number) => map.set(num, (map.get(num) || 0) + count)
